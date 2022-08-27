df17dcfab81fdd1ff1b204e9978a273a4deeae0e
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-27 07:01:45 -0400

    Add notes to json

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const { notes } = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// // parse incoming string or array data
-// app.use(express.urlencoded({ extended: true }));
-// // parse incoming JSON data
-// app.use(express.json());
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 // app.use(express.static('public'));
 
 // app.use('/api', apiRoutes);
@@ -30,10 +30,34 @@ app.get('/api/notes/:id', (req, res) => {
     }
 });
 
+app.post('/api/notes', (req, res) => {
+    // req.body is incoming content
+    req.body.id = notes.length.toString();
+
+    // add notes to json file and notes array
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+});
+
 function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
     return result;
 }
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+}
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
