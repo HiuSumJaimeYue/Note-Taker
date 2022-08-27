171fa0eb700bd1d3fdc0e00feab58a99240a3a90
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-27 06:38:14 -0400

    FindById

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
