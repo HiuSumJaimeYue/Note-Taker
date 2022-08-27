81ab40688b0ceefa8f2cc0910eec4f59b0751310
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-27 06:21:59 -0400

    port variable

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { db } = require('./db/db.json');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-const { db } = require('./db/db.json');
+// // parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// // parse incoming JSON data
+// app.use(express.json());
+// app.use(express.static('public'));
+
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 app.get('/api/db', (req, res) => {
     res.json(db);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
