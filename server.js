5ca08d26524cabe2d3921b025132a5952f0ad445
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-27 07:07:00 -0400

    validateNote

--- server.js
+++ server.js
@@ -34,10 +34,14 @@ app.post('/api/notes', (req, res) => {
     // req.body is incoming content
     req.body.id = notes.length.toString();
 
-    // add notes to json file and notes array
-    const note = createNewNote(req.body, notes);
-
-    res.json(note);
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        // add notes to json file and notes array
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 function findById(id, notesArray) {
@@ -57,7 +61,15 @@ function createNewNote(body, notesArray) {
     return note;
 }
 
-
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
