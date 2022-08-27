88f44afb57e9726a1b4492f1cbbaa7f646895e94
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-27 07:27:20 -0400

    Add routes of HTML (notes and index)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
@@ -70,6 +70,18 @@ function validateNote(note) {
     }
     return true;
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
